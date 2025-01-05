ada5e235dda7a53ba335cf97b651ab6c668afbc7
Akarsh404 <151439747+Akarsh404@users.noreply.github.com>
2025-01-05 15:51:18 +0530

    Update tempCodeRunnerFile.js

--- basics_01/tempCodeRunnerFile.js
+++ basics_01/tempCodeRunnerFile.js
@@ -1,1 +1,18 @@
-arshsi
+function convert(){
+input=document.getElementById("input").value;
+ctof=document.getElementById("ctof").checked;
+ftoc=document.getElementById("ftoc").checked;
+result=document.getElementById("result");
+let temp;
+    if (ctof){
+        temp= (number(input) * 9/5) + 32;
+result.textContent=`${temp}`;
+    }
+else if(ftoc){
+     temp=(number(input)-32)*5/9;
+   result.textContent=`${temp}`;
+  }
+
+else 
+result= "select a conversion";
+}
